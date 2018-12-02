234ae9d734235a555f17125822c2ec62bdb48de6
talha <talhamucar@gmail.com>
2018-12-02 16:15:32 -0500

    Components are added

--- soccer-app/src/app/app.module.ts
+++ soccer-app/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlayersComponent } from './players/players.component';
+import { PitchComponent } from './players/pitch/pitch.component';
+import { PlayerListsComponent } from './players/player-lists/player-lists.component';
+import { GkComponent } from './players/player-lists/gk/gk.component';
+import { DrComponent } from './players/player-lists/dr/dr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlayersComponent,
+    PitchComponent,
+    PlayerListsComponent,
+    GkComponent,
+    DrComponent
   ],
   imports: [
     BrowserModule
